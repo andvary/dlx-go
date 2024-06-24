6041902e11b434999104c8dfadde061ed894dc13
aberdnikov <a.berdnikov@infinet.ru>
2024-06-24 19:23:24 +0500

    +nrooks test

--- nrooks/nrooks_test.go
+++ nrooks/nrooks_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestNRooks(t *testing.T) {
+func TestNRooks_8(t *testing.T) {
 	m := prepareMatrix(8, 8)
 	d, err := dlx.New(m)
 	if err != nil {
@@ -22,4 +22,23 @@ func TestNRooks(t *testing.T) {
 	}
 
 	printBoard(8, 8, solutions[3100:3104], prepareMatrix(8, 8))
+}
+
+func TestNRooks_10(t *testing.T) {
+	m := prepareMatrix(10, 10)
+	d, err := dlx.New(m)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	solutions, err := d.Solve()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(solutions) != 3628800 {
+		t.Fatal(err)
+	}
+
+	printBoard(10, 10, solutions[3100:3104], prepareMatrix(10, 10))
 }
